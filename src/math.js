72ec95242af50064a1d40e876fc69c4c0f644ffb
Clundy90 <roetemeteor2@gmail.com>
2026-02-20 13:08:48 -0600

    added new test cases

--- src/math.js
+++ src/math.js
@@ -1,8 +1,21 @@
 export const add = (a, b) => a + b;
 export const subtract = (a, b) => a - b;
-export const multiply = (a, b) => a * b;
+export const multiply = (a, b) => {
+  // console.log("a", a);
+  // console.log("b", b);
+  market.getApples();
+  if (isNaN(a) || isNaN(b)) {
+    return "You need to enter a valid number";
+  }
+  market.getApples();
+  return a * b;
+};
 
 export const divide = (a, b) => {
   if (b === 0) return "Error: You cannot divide by 0";
   return a / b;
+};
+
+export const market = {
+  getApples: (count) => 100 + count,
 };
--- src/math.test.js
+++ src/math.test.js
@@ -1,6 +1,8 @@
 import { expect, test, describe } from "vitest";
-import { add, subtract, multiply, divide } from "./math";
+import { add, subtract, multiply, divide, market } from "./math";
+import { vi } from "vitest";
 
+const marketSpy = vi.spyOn(market, "getApples");
 describe("Calculator Logic", () => {
   test("adds 10 + 5 to equal 15", () => {
     expect(add(10, 5)).toBe(15);
@@ -19,5 +21,17 @@ describe("Calculator Logic", () => {
   });
   test("returns an error message when dividing by zero", () => {
     expect(divide(10, 0)).toBe("Error: You cannot divide by 0");
+  });
+  test("if it is not a number", () => {
+    expect(multiply(Number("text"), "")).toEqual(
+      "You need to enter a valid number",
+    );
+  });
+
+  test("calls apple", () => {
+    expect(multiply(1, 2)).toEqual(2);
+    expect(marketSpy).toBeCalledTimes(5);
+    const mocker = marketSpy.mockImplementation();
+    expect(mocker(2)).toEqual(102);
   });
 });
